ef054ecd456064d3b8ae1b68f612099badb2f581
Jeevaa JK <jeevaajk2007@gmail.com>
2025-09-13 21:46:06 +0530

    Add files via upload
    A program to find the Body Mass Index and the height is in cm and weight in kg

---
+++ bmi.py
@@ -0,0 +1,5 @@
+#A program to find the Body Mass Index and the height is in cm and weight in kg
+height=float(input("Enter Your Height : "))
+weight=float(input("Enter Your Weight : "))
+bmi=weight/(height**2)
+print("Your Body Mass Index is ",bmi)
