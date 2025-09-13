af05d18f8cde5a60081457d4ce2bda654b8a4a5e
Jeevaa JK <jeevaajk2007@gmail.com>
2025-09-13 20:47:14 +0530

    Add files via upload
    Just my first assignment in udemy

---
+++ brandname.py
@@ -0,0 +1,4 @@
+#A program that gives you a brand name combining your city name and your pet name
+name=input("Enter the city where You born : \n")
+pet_name=input("Enter Your pet name : \n")
+print(name+pet_name)
